6cc50ffc6c07eebfe36b8086c72f1d3ffe30b5a2
Hiroki <ll@lls-macbook-pro.local>
2019-03-10 15:42:36 -0400

    Initial Commit

---
+++ WeatherGift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherGift
+//
+//  Created by Hiroki on 2019/03/10.
+//  Copyright © 2019 Hiroki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
